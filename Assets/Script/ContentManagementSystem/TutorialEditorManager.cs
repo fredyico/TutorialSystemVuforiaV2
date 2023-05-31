4e29add1a5a44b932949fca5f44df050b8c314f9
fredyico <118931952+fredyico@users.noreply.github.com>
2023-05-31 01:21:21 -0300

    update image and video button

--- Assets/Script/ContentManagementSystem/TutorialEditorManager.cs
+++ Assets/Script/ContentManagementSystem/TutorialEditorManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.IO;
+using SimpleFileBrowser; // File Browser namespace
+using UnityEngine.Video;
 
 public class TutorialEditorManager : MonoBehaviour
 {
@@ -16,6 +19,10 @@ public class TutorialEditorManager : MonoBehaviour
     public TextMeshProUGUI tabText;
     public TextMeshProUGUI textContentText;
 
+    // Raw Image and Video Player Objects from Preview Tutorial Panel
+    public RawImage previewImage;
+    public VideoPlayer previewVideoPlayer;
+
     void Start()
     {
         titleInputField.onValueChanged.AddListener(OnTitleChanged);
@@ -27,15 +34,56 @@ public class TutorialEditorManager : MonoBehaviour
     {
         titleText.text = newValue;
     }
-
     public void OnTabChanged(string newValue)
     {
         tabText.text = newValue;
     }
-
     public void OnTextContentChanged(string newValue)
     {
         textContentText.text = newValue;
     }
+    public void OnImageButtonPressed()
+    {
+        StartCoroutine(ShowLoadImageDialogCoroutine());
+    }
+    public void OnVideoButtonPressed()
+    {
+        StartCoroutine(ShowLoadVideoDialogCoroutine());
+    }
 
+    private IEnumerator ShowLoadImageDialogCoroutine()
+    {
+        // Show a load file dialog and wait for a response from user
+        // Load file/folder: FileBrowser.ShowLoadDialog( OnSuccess, OnCancel )
+        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Files);
+
+        // FileBrowser.Success returns true when a file/folder is picked
+        // If so, output the file/folder path
+        if (FileBrowser.Success)
+        {
+            string filePath = FileBrowser.Result[0];
+
+            // Load the image into a texture
+            byte[] imageBytes = File.ReadAllBytes(filePath);
+            Texture2D tex = new Texture2D(2, 2);
+            tex.LoadImage(imageBytes);
+
+            // Apply the texture to the preview image
+            previewImage.texture = tex;
+        }
+    }
+    private IEnumerator ShowLoadVideoDialogCoroutine()
+    {
+        yield return FileBrowser.WaitForLoadDialog(FileBrowser.PickMode.Files);
+
+        if (FileBrowser.Success)
+        {
+            string filePath = FileBrowser.Result[0];
+
+            // Load the video from the file path
+            previewVideoPlayer.url = filePath;
+            previewVideoPlayer.Prepare();
+            previewVideoPlayer.Play();
+        }
+    }
 }
